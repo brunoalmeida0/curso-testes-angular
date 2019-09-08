e527dc97def4b4a4a33aa7d53c522f71eb4d1f8a
Bruno Almeida <brunoalmeida356@gmail.com>
2019-09-08 09:27:42 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redesocial-cursotestes';
+
+  ngOnInit() {
+    let teste = 123
+    let teste2 = 34
+
+    let soma = teste + teste2
+
+    console.log(soma)
+  }
 }
